8159af841b039c4c62efaa53cf6bdf45c15d2ec5
zhangchunhui <zhangchunhui@huobi.com>
2020-06-01 16:41:15 +0800

    material ui

--- reack-pm0/src/App.js
+++ reack-pm0/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import A1 from './_components/a1/A1';
 
+// https://medium.com/codingthesmartway-com-blog/getting-started-with-material-ui-for-react-material-design-for-react-364b2688b555
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+
 class App extends Component {
 
   constructor(props) {
@@ -14,6 +19,13 @@ class App extends Component {
     return (
       <div className="App">
         <A1>a1</A1>
+        <AppBar position="static">
+            <Toolbar>
+                <Typography color="inherit">
+                React & Material-UI Sample Application
+                </Typography>
+            </Toolbar>
+        </AppBar>
       </div>
     );
   }
