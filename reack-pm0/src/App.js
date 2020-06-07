8817af4babd0c292df230b8c98c617c9e380ff7d
zhangchunhui <chunhui2001@gmail.com>
2020-06-08 01:01:05 +0800

    udpate tabs

--- reack-pm0/src/App.js
+++ reack-pm0/src/App.js
@@ -5,13 +5,40 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      items: []
+    };
+  }
+
+  onButtonClickHand(pm, type, inputTextValue) {
+    this.getList(pm);
+    pm.refresh();
+  }
+
+  put() {
+    let _items = this.state.items;
+    _items.push(new Date().getTime());
+    this.setState({
+      ...this.state.items,
+      items: _items
+    });
+    this.getList(this.refs.pm0);
+    this.refs.pm0.refresh();
+  }
+
+  getList(e) {
+    e.responseComponent = <div>
+      { this.state.items.map((item, index) => {
+        return <p key={index}>{item}</p>
+      }) }
+    </div>;
   }
 
   render() {
     return (
       <div className='pm-app'>
-          <PM0 />
+          <input type="button" onClick={this.put.bind(this)} value="Test" />
+          <PM0 ref="pm0" onButtonClickHand={this.onButtonClickHand.bind(this)} />
       </div>
     );
   }
--- reack-pm0/src/_components/pm0/PM0.js
+++ reack-pm0/src/_components/pm0/PM0.js
@@ -1,133 +1,196 @@
 import React, { Component } from 'react';
-
 import styled, { css } from "styled-components";
 
 export class _PM0 extends Component {
 
+  responseComponent = null;
+
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      tabName: 'params',
+      pmForm: {
+        inputTextValue: ''
+      }
+    };
   }
 
-handleInputTextChange (e) {
-  let keyArr = e.target.name.split('.');
-  if (keyArr.length === 1) {
-    this.setState({ 
-      ...this.state,
-      task: {
-        ...this.state.task,
-        [keyArr[0]]: e.target.value
-      }
-    });
-  } else if (keyArr.length === 2) {
-    this.setState({ 
-      ...this.state,
-      task: {
-        ...this.state.task,
-        taskDef: {
-          ...this.state.task.taskDef,
-          [keyArr[1]]: e.target.value
+  handleInputTextChange (e) {
+    let keyArr = e.target.name.split('.');
+    if (keyArr.length === 1) {
+      this.setState({ 
+        ...this.state,
+        pmForm: {
+          ...this.state.pmForm,
+          [keyArr[0]]: e.target.value
         }
+      });
+    } else if (keyArr.length === 2) {
+      this.setState({ 
+        ...this.state,
+        task: {
+          ...this.state.task,
+          taskDef: {
+            ...this.state.task.taskDef,
+            [keyArr[1]]: e.target.value
+          }
+        }
+      });
+    }
+  }
+
+  handPmTabClick(tabName, e) {
+    this.setState({
+      ...this.state,
+      tabName: tabName
+    }) 
+  }
+
+  onSendClick() {
+
+  }
+
+  onButtonClick(type, e) {
+    const { onButtonClickHand } = this.props;
+    let _inputTextValue = this.state.pmForm.inputTextValue;
+    if (!_inputTextValue || !_inputTextValue.trim()) {
+      return;
+    }
+    if (!onButtonClickHand) {
+      return;
+    }
+    onButtonClickHand(this, type, _inputTextValue);
+  }
+
+  getResponseComponent() {
+      if (this.responseComponent) {
+          return this.responseComponent;
       }
+      return <h1>888</h1>;
+  }
+
+  refresh() {
+    this.setState({
+      ...this.state,
+      timestamp: new Date().getTime()
     });
   }
-}
 
-handPmTabClick() {
-  alert(1);
-}
+  render() {
 
-render() {
-  return (
-      <div className={`${this.props.className} PM0`}>
-      	<div className={'pm-body'}>
-          <input style={{width:'465px'}} type="text" />
-          <input type="button" value="Send" />
-          <input style={{marginRight: 0}} type="button" value="ping" />
-      	</div>
-        <div className={'pm-m'}>
-        	<div className={'pm-tab'}>
-        		<span className={'active'} onClick={this.handPmTabClick.bind(this)}>Params</span>
-        		<span onClick={this.handPmTabClick.bind(this)}>Authorization</span>
-        		<span onClick={this.handPmTabClick.bind(this)}>Headers(1)</span>
-        		<span onClick={this.handPmTabClick.bind(this)}>Body</span>
+    const { 
+      pmForm
+    } = this.state;
+
+    return (
+        <div className={`${this.props.className} PM0`}>
+        	<div className={'pm-body'}>
+            <input style={{width:'465px'}} type="text" name='inputTextValue' 
+                  onChange={(e) => {this.handleInputTextChange(e)}} value={pmForm.inputTextValue || ''} />
+            <input onClick={this.onButtonClick.bind(this, 'send')} disabled={!pmForm.inputTextValue.trim()} type="button" value="Send" />
+            <input onClick={this.onButtonClick.bind(this, 'ping')} disabled={!pmForm.inputTextValue.trim()} style={{marginRight: 0}} type="button" value="ping" />
         	</div>
-        	<div className={'tab-panel tab-params'}>
-        		<div style={{color:'gray', padding: '0.425em .625em', fontStyle: 'italic', backgroundColor: 'mintcream'}}>Query Params</div>
-        		<table className={'query-params-table'}>
-        			<tbody>
-        				<tr>
-        					<th style={{width:'25px'}}>&nbsp;</th>
-        					<th>KEY</th>
-        					<th>VALUE</th>
-        					<th>DESCRIPTION</th>
-        				</tr>
-        				<tr>
-        					<td style={{textAlign:'right'}}><input type="checkbox" /></td>
-        					<td>&nbsp;</td>
-        					<td>&nbsp;</td>
-        					<td>&nbsp;</td>
-        				</tr>
-        			</tbody>
-        		</table>
-        	</div>
-          <div className={'tab-panel tab-auth'} style={{display:'none'}}>
-            <div className={'left'} style={{width:'30%', backgroundColor:'azure'}}>
-              <div>
-                1
+          <div className={'pm-m'}>
+          	<div className={'pm-tab'}>
+          		<span className={this.state.tabName === 'params' ? 'active' : ''} onClick={this.handPmTabClick.bind(this, 'params')}>Params</span>
+          		<span className={this.state.tabName === 'auth' ? 'active' : ''} onClick={this.handPmTabClick.bind(this, 'auth')}>Authorization</span>
+          		<span className={this.state.tabName === 'headers' ? 'active' : ''} onClick={this.handPmTabClick.bind(this, 'headers')}>Headers(1)</span>
+          		<span className={this.state.tabName === 'body' ? 'active' : ''} onClick={this.handPmTabClick.bind(this, 'body')}>Body</span>
+          	</div>
+          	{ this.state.tabName === 'params' && this.paramsTabSection() }
+            { this.state.tabName === 'auth' && this.authTabSection() }
+            { this.state.tabName === 'headers' && this.headersTabSection() }
+            { this.state.tabName === 'body' && this.bodyTabsSection() }
+            <div className={'pm-response'}>
+              <h4 style={{padding:'.525em .625em',margin:0, backgroundColor:'aliceblue', color:'gray', borderBottom:'solid 1px gainsboro'}}>Response</h4>
+              <div style={{ backgroundColor: 'darkslateblue', color:'white', minHeight:'300px' }}>
+                { this.getResponseComponent() }
               </div>
             </div>
-            <div className={'right'} style={{width:'70%', backgroundColor:'ghostwhite'}}>
-              <div>
-                3
-              </div>
-            </div>
-            <div className={'clear'}></div>
           </div>
-          <div className={'tab-panel tab-headers'} style={{display:'none'}}>
-            <div style={{color:'gray', padding: '0.425em .625em', fontStyle: 'italic', backgroundColor: 'lavender'}}>Headers</div>
-            <table className={'query-params-table'}>
-              <tbody>
-                <tr>
-                  <th style={{width:'25px'}}>&nbsp;</th>
-                  <th>KEY</th>
-                  <th>VALUE</th>
-                  <th>DESCRIPTION</th>
-                </tr>
-                <tr>
-                  <td style={{textAlign:'right'}}><input type="checkbox" /></td>
-                  <td>&nbsp;</td>
-                  <td>&nbsp;</td>
-                  <td>&nbsp;</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <div className={'tab-panel tab-bodys'} style={{display:'none'}}>
-            <div style={{color:'gray', padding: '0.425em .625em', backgroundColor: 'lavender'}}>
-              <span><input name="g" id="r-none" type="radio" /><label htmlFor="r-none">NONE</label></span>
-              <span><input name="g" id="r-form-data" type="radio" /><label htmlFor="r-form-data">form-data</label></span>
-              <span><input name="g" id="r-form-urlencoded" type="radio" /><label htmlFor="r-form-urlencoded">x-www-form-urlencoded</label></span>
-              <span><input name="g" id="r-form-raw" type="radio" /><label htmlFor="r-form-raw">raw</label></span>
-              <span><input name="g" id="r-form-binary" type="radio" /><label htmlFor="r-form-binary">binary</label></span>
-              <div className={'clear'}></div>
-            </div>
-            <div style={{padding:'1em'}}>
-              1 <br />
-              1 <br />
-              1 <br />
-              1 <br />
-            </div>
-          </div>
-          <div className={'pm-response'}>
-            <h4 style={{padding:'.525em .625em',margin:0, backgroundColor:'aliceblue', color:'gray', borderBottom:'solid 1px gainsboro'}}>Response</h4>
-            <div style={{padding:'1em', backgroundColor: 'darkslateblue', color:'white',minHeight:'300px'}}>
-              66
-            </div>
-          </div>
+          <div style={{display:'none'}}>{this.state.timestamp}</div>
         </div>
-      </div>
     );
+  }
+
+
+  paramsTabSection() {
+    return <div className={'tab-panel tab-params'}>
+              <div style={{color:'gray', padding: '0.425em .625em', fontStyle: 'italic', backgroundColor: 'mintcream'}}>Query Params</div>
+              <table className={'query-params-table'}>
+                <tbody>
+                  <tr>
+                    <th style={{width:'25px'}}>&nbsp;</th>
+                    <th>KEY</th>
+                    <th>VALUE</th>
+                    <th>DESCRIPTION</th>
+                  </tr>
+                  <tr>
+                    <td style={{textAlign:'right'}}><input type="checkbox" /></td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>;
+  }
+
+  authTabSection() {
+    return <div className={'tab-panel tab-auth'}>
+              <div className={'left'} style={{width:'30%', backgroundColor:'azure'}}>
+                <div>
+                  1
+                </div>
+              </div>
+              <div className={'right'} style={{width:'70%', backgroundColor:'ghostwhite'}}>
+                <div>
+                  3
+                </div>
+              </div>
+              <div className={'clear'}></div>
+            </div>;
+  }
+
+  bodyTabsSection() {
+    return <div className={'tab-panel tab-bodys'}>
+              <div style={{color:'gray', padding: '0.425em .625em', backgroundColor: 'lavender'}}>
+                <span><input name="g" id="r-none" type="radio" /><label htmlFor="r-none">NONE</label></span>
+                <span><input name="g" id="r-form-data" type="radio" /><label htmlFor="r-form-data">form-data</label></span>
+                <span><input name="g" id="r-form-urlencoded" type="radio" /><label htmlFor="r-form-urlencoded">x-www-form-urlencoded</label></span>
+                <span><input name="g" id="r-form-raw" type="radio" /><label htmlFor="r-form-raw">raw</label></span>
+                <span><input name="g" id="r-form-binary" type="radio" /><label htmlFor="r-form-binary">binary</label></span>
+                <div className={'clear'}></div>
+              </div>
+              <div style={{padding:'1em'}}>
+                1 <br />
+                1 <br />
+                1 <br />
+                1 <br />
+              </div>
+            </div>;
+  }
+
+  headersTabSection() {
+    return <div className={'tab-panel tab-headers'}>
+              <div style={{color:'gray', padding: '0.425em .625em', fontStyle: 'italic', backgroundColor: 'lavender'}}>Headers</div>
+              <table className={'query-params-table'}>
+                <tbody>
+                  <tr>
+                    <th style={{width:'25px'}}>&nbsp;</th>
+                    <th>KEY</th>
+                    <th>VALUE</th>
+                    <th>DESCRIPTION</th>
+                  </tr>
+                  <tr>
+                    <td style={{textAlign:'right'}}><input type="checkbox" /></td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                    <td>&nbsp;</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>;
   }
 }
 
@@ -138,6 +201,13 @@ let mixin = css`&{
   .clear {
     float: none;
     clear: both;
+  }
+  .pm-tab > span {
+      -webkit-user-select: none;
+      -khtml-user-select: none;
+      -moz-user-select: -moz-none;
+      -o-user-select: none;
+      user-select: none;
   }
 	.pm-body {
 		margin: .5em 0;
@@ -168,7 +238,6 @@ let mixin = css`&{
     .pm-tab span.active {
     	border-bottom:solid 3px red;
     	margin-bottom: -1px;
-      font-weight:bold;
     }
     .tab-panel {
     	background-color: floralwhite;
