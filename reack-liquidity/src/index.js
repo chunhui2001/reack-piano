2f4606220e4c651cf9f5d2bcbd84d829593ae871
zhangchunhui <zhangchunhui@huobi.com>
2020-07-15 19:42:47 +0800

    a
    a

--- reack-liquidity/src/index.js
+++ reack-liquidity/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Fake } from 'reack-fake';
+
+const ReactDOM = Fake('ReactDOM');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
