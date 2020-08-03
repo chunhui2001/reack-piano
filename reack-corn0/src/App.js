0ec00ba76a9b4a612d2a761bfcb10082f74dfd88
zhangchunhui <zhangchunhui@huobi.com>
2020-08-03 16:46:21 +0800

    a
    a

--- reack-corn0/src/App.js
+++ reack-corn0/src/App.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import CornExp from './_components/corn0/CornExp';
 
 class App extends Component {
-
+  
   constructor(props) {
 
     super(props);
-    this.state = {};
+    this.state = { value1: "* * * * *", value2: "* * * * *" };
 
   }
 
   render() {
     return (
       <div className="App">
-        <CornExp>corn0</CornExp>
+          <CornExp locale={"zh_CN"} value={this.state.value2} onChange={value => this.setState({ value2: value })}/>
       </div>
     );
   }
--- reack-corn0/src/_components/corn0/CornExp.js
+++ reack-corn0/src/_components/corn0/CornExp.js
@@ -1,29 +1,263 @@
-import React from 'react';
-// Styled components 💅 is library that allows you create components easily and
-// style them, css-in-js style. It's totally optional, but I prefer to use it
-// to build things quickly and have them look great every step of the way.
-import styled from 'styled-components';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import cronstrue from 'cronstrue/i18n';
+import Cron from "cron-converter";
+import valueHints from './valueHints';
+import './crontab-input.css';
+import locales from "./locales";
 
-// Style components take CSS in a template string. Even Sass functions with work!
-// Each element is a property of styled, like h3, p, div, etc...
-const CornExpWrapper = styled.button`
-	border-radius: 8px;
-	color: #fff;
-	background: green;
-	padding: 8px 15px;
-	border: none;
-	outline: none;
-	border-radius: 0;
-	cursor: pointer;
-`;
+class CornExp extends Component {
+  state = {
+    parsed: {},
+    highlightedExplanation: "",
+    isValid: true,
+    selectedPartIndex: -1,
+    nextSchedules: [],
+    nextExpanded: false,
+  };
+  inputRef;
 
-// Components are functions, and they must start with a capital letter
-function CornExp(props) {
-	// {...props} uses the the ES6 spread operator to send any props you may pass
-	// along without changing any of the contents. This is basically just creating
-	// a copy to pass along
-	return <CornExpWrapper {...props}>{props.children}</CornExpWrapper>;
+  lastCaretPosition = -1;
+
+  UNSAFE_componentWillMount() {
+    this.calculateNext();
+    this.calculateExplanation();
+  }
+
+  clearCaretPosition() {
+    this.lastCaretPosition = -1;
+    this.setState({
+      selectedPartIndex: -1,
+      highlightedExplanation: this.highlightParsed(-1),
+    });
+  }
+
+  calculateNext() {
+    let nextSchedules = [];
+    try {
+      let cronInstance = new Cron();
+      cronInstance.fromString(this.props.value);
+      let timePointer = +new Date();
+      for (let i = 0; i < 5; i++) {
+        let schedule = cronInstance.schedule(timePointer);
+        let next = schedule.next();
+        nextSchedules.push(next.format("YYYY-MM-DD HH:mm:ss"));
+        timePointer = +next + 1000;
+      }
+    } catch (e) {
+    }
+    this.setState({ nextSchedules });
+  }
+
+  highlightParsed(selectedPartIndex) {
+    let parsed = this.state.parsed;
+    if (!parsed) {
+      return;
+    }
+    let toHighlight = [];
+    let highlighted = "";
+
+    for (let i = 0; i < 5; i++) {
+      if (parsed.segments[i] && parsed.segments[i].text) {
+        toHighlight.push({...parsed.segments[i]});
+      } else {
+        toHighlight.push(null);
+      }
+    }
+
+    if (selectedPartIndex >= 0) {
+      if (toHighlight[selectedPartIndex]) {
+        toHighlight[selectedPartIndex].active = true;
+      }
+    }
+
+    // handle special case where minute/hour is presented in the same segment
+    if (toHighlight[0] && toHighlight[1] && toHighlight[0].text && toHighlight[0].text === toHighlight[1].text && toHighlight[0].start === toHighlight[1].start) {
+      if (toHighlight[1].active) {
+        toHighlight[0] = null;
+      } else {
+        toHighlight[1] = null;
+      }
+    }
+
+    toHighlight = toHighlight.filter(_ => _);
+
+    toHighlight.sort((a, b) => {
+      return a.start - b.start;
+    });
+    
+    let pointer = 0;
+    toHighlight.forEach(item => {
+      if (pointer > item.start) {
+        return;
+      }
+      highlighted += parsed.description.substring(pointer, item.start);
+      pointer = item.start;
+      highlighted += `<span${item.active ? ' class="active"' : ''}>${parsed.description.substring(pointer, pointer + item.text.length)}</span>`;
+      pointer += item.text.length;
+    });
+
+    highlighted += parsed.description.substring(pointer);
+
+    return highlighted;
+  }
+
+  calculateExplanation(callback) {
+    let isValid = true;
+    let parsed;
+    let highlightedExplanation;
+    try {
+      parsed = cronstrue.parse(this.props.value, { locale: this.props.locale });
+      highlightedExplanation = "";
+
+    } catch (e) {
+      highlightedExplanation = e.toString();
+      isValid = false;
+    }
+    this.setState({
+      parsed,
+      highlightedExplanation,
+      isValid,
+    }, () => {
+      if (isValid) {
+        this.setState({
+          highlightedExplanation: this.highlightParsed(-1),
+        });
+      }
+      if (callback) {
+        callback();
+      }
+    });
+  }
+
+  onCaretPositionChange() {
+    if (!this.inputRef) {
+      return;
+    }
+    let caretPosition = this.inputRef.selectionStart;
+    let selected = this.props.value.substring(this.inputRef.selectionStart, this.inputRef.selectionEnd);
+    if (selected.indexOf(" ") >= 0) {
+      caretPosition = -1;
+    }
+    if (this.lastCaretPosition === caretPosition) {
+      return;
+    }
+    this.lastCaretPosition = caretPosition;
+    if (caretPosition === -1) {
+      this.setState({
+        highlightedExplanation: this.highlightParsed(-1),
+        selectedPartIndex: -1,
+      });
+      return;
+    }
+    let textBeforeCaret = this.props.value.substring(0, caretPosition);
+    let selectedPartIndex = textBeforeCaret.split(" ").length - 1;
+    this.setState({
+      highlightedExplanation: this.highlightParsed(selectedPartIndex),
+      selectedPartIndex,
+    });
+  }
+
+  getLocale() {
+    return locales[this.props.locale];
+  }
+
+  UNSAFE_componentWillReceiveProps(nextProps) {
+    if (nextProps.value !== this.props.value) {
+      setTimeout(() => {
+        this.calculateExplanation(() => {
+          this.onCaretPositionChange();
+          this.calculateNext();
+        });
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="crontab-input">
+
+
+        <input type="text" className={"cron-input " + (!this.state.isValid ? "error" : "")}
+               value={this.props.value}
+               ref={ref => {
+                 this.inputRef = ref;
+               }}
+               onMouseUp={e => {
+                 this.onCaretPositionChange()
+               }}
+               onKeyUp={e => {
+                 this.onCaretPositionChange()
+               }}
+               onBlur={e => {
+                 this.clearCaretPosition()
+               }}
+               onChange={e => {
+                 let parts = e.target.value.split(" ").filter(_ => _);
+                 if (parts.length !== 5) {
+                   this.props.onChange(e.target.value);
+                   this.setState({
+                     parsed: {},
+                     isValid: false,
+                   });
+                   return;
+                 }
+
+                 this.props.onChange(e.target.value);
+               }}/>
+
+
+        <div className="next">
+          {!!this.state.nextSchedules.length && <span>
+            {this.getLocale().nextTime}: {this.state.nextSchedules[0]} {this.state.nextExpanded ?
+            <a onClick={() => this.setState({ nextExpanded: false })}>({this.getLocale().hide})</a> :
+            <a onClick={() => this.setState({ nextExpanded: true })}>({this.getLocale().showMore})</a>}
+            {!!this.state.nextExpanded && <div className="next-items">
+              {this.state.nextSchedules.slice(1).map((item, index) => <div
+                className="next-item" key={index}>{this.getLocale().then}: {item}</div>)}
+            </div>}
+          </span>}
+        </div>
+
+        <div className="parts">
+          {[
+            this.getLocale().minute,
+            this.getLocale().hour,
+            this.getLocale().dayMonth,
+            this.getLocale().month,
+            this.getLocale().dayWeek,
+          ].map((unit, index) => (
+            <div key={index}
+                 className={"part " + (this.state.selectedPartIndex === index ? "selected" : "")}>{unit}</div>
+          ))}
+        </div>
+
+        {valueHints[this.props.locale][this.state.selectedPartIndex] && <div className="allowed-values">
+          {valueHints[this.props.locale][this.state.selectedPartIndex].map((value, index) => (
+            <div className="value" key={index}>
+              <div className="key">{value[0]}</div>
+              <div className="value">{value[1]}</div>
+            </div>
+          ))}
+        </div>}
+
+        
+        <div className="explanation"
+             dangerouslySetInnerHTML={{ __html: this.state.isValid ? `“${this.state.highlightedExplanation}”` : "　" }}/>
+      </div>
+    );
+  }
 }
 
-// This export will be picked up in ./index.js
+CornExp.propTypes = {
+  locale: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+CornExp.defaultProps = {
+  locale: "en",
+};
+
 export default CornExp;
+
